d0c95be260bb36a4ed7d308061b4626cb947ab4f
davidpalacios <palaciosdavid@comcast.net>
2021-05-26 06:26:37 -0500

    commands folder structure and custom prefix

--- bot.js
+++ bot.js
@@ -1,17 +1,54 @@
-require('dotenv').config()
+const fs = require('fs');
+const { prefix, bot_token } = require('./config.json');
 
 const Discord = require('discord.js');
 const client = new Discord.Client({
     partials: ["MESSAGE"]
 });
+client.commands = new Discord.Collection();
 
-client.on('ready', () => {
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    // set a new item in the Collection
+    // with the key as the command name and the value as the exported module
+    client.commands.set(command.name, command);
+}
+
+client.once('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-    if (msg.content === 'ping') {
-        msg.reply('Pong!');
+client.on('message', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    //regex to avoid spaces as args
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const commandName = args.shift().toLowerCase();
+
+    if (!client.commands.has(commandName)) {
+        message.reply('command not found!');
+        return;
+    }
+
+    const command = client.commands.get(commandName);
+    if (command.args && !args.length) {
+        let reply = `You didn't provide any arguments, ${message.author}!`;
+
+        if (command.usage) {
+            reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
+        }
+
+        return message.channel.send(reply);
+    }
+
+    try {
+        command.execute(message, args);
+    }
+    catch (error) {
+        console.error(error);
+        message.reply('there was an error trying to execute that command!');
     }
 });
 
@@ -21,4 +58,4 @@ client.on('message', msg => {
     }
 })
 
-client.login(process.env.BOT_TOKEN);
+client.login(bot_token);
